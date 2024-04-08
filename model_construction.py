de827071ae9a8b05026d1722657d1c99f1418cfe
BruceZhangReve <121945352+BruceZhangReve@users.noreply.github.com>
2024-04-08 21:08:15 +0800

    Add files via upload

--- model_construction.py
+++ model_construction.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv,GATConv
 import gzip
 import pickle
 
-# Model Construction
+# Model Constructions
+
 class GCNPair(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super().__init__()
@@ -23,6 +24,30 @@ class GCNPair(torch.nn.Module):
         x_r = self.conv1_r(graph_r.x, graph_r.edge_index).relu()
         x_r = F.dropout(x_r, p=0.6, training=self.training)
         x_r = self.conv2_r(x_r, graph_r.edge_index)
+
+        x_merge = merge_graphs(x_l, x_r, labels)
+        x = F.relu(self.fc1(x_merge))
+        out = self.fc2(x)
+        return out
+
+class GATPair(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, heads=2):
+        super().__init__()
+        self.conv1_l = GATConv(input_dim, hidden_dim, heads=heads, concat=False)
+        self.conv2_l = GATConv(hidden_dim, hidden_dim, heads=heads, concat=False)
+        self.conv1_r = GATConv(input_dim, hidden_dim, heads=heads, concat=False)
+        self.conv2_r = GATConv(hidden_dim, hidden_dim, heads=heads, concat=False)
+        self.fc1 = torch.nn.Linear(hidden_dim*2, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, graph_l, graph_r, labels):
+        x_l = self.conv1_l(graph_l.x, graph_l.edge_index).relu()
+        x_l = F.dropout(x_l, p=0.6, training=self.training)
+        x_l = self.conv2_l(x_l, graph_l.edge_index).relu()
+
+        x_r = self.conv1_r(graph_r.x, graph_r.edge_index).relu()
+        x_r = F.dropout(x_r, p=0.6, training=self.training)
+        x_r = self.conv2_r(x_r, graph_r.edge_index).relu()
 
         x_merge = merge_graphs(x_l, x_r, labels)
         x = F.relu(self.fc1(x_merge))
